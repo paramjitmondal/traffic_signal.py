3908653aee2e3096b065e9cacc617ef42374ee04
Paramjit Mondal <paramjitmondal32@gmail.com>
2026-07-29 08:10:58 +0530

    Add files via upload

---
+++ signal.py
@@ -0,0 +1,9 @@
+signal=(input("enter your colur:"))
+if signal   =='red':
+    print("stopppppppppp")
+elif signal =='green':
+    print("goooooooo")
+elif signal =='yellow':
+    print("readyyyyyy")
+else:
+    print("invalid colur...please enter a valid colur [red,yellow or green]")
